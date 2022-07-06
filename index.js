492036dfc3df8eea82469dce891c62d0386dba93
tamunosakijonah <tamunosakijonah@gmail.com>
2022-07-06 22:39:37 +0100

    alert added

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var operator = prompt("enter operator either +,-.*,/,");
 var num1=prompt("enter first number");
 var num2=prompt("enter second number");
-num1=parseFloat(num1);
-num2=parseFloat(num2);
-alert(num1*num2);
+let result;
+if(operator=="+"){
+    result=num1 + num2;
+}else if(operator=="-"){
+    result = num1 - num2
+}
+else if(operator=="*"){
+    result = num1 * num2
+}
+else if(operator=="/"){
+    result = num1 / num2
+}
+
+console.log(alert(`${num1}${operator}${num2}=${result}`));
